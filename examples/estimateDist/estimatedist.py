8c38e74492a69acbd8416b2dcfdeabf6c550e1e3
M. Pilosov <michael.pilosov@ucdenver.edu>
2016-06-22 18:09:17 -0600

    actually (this time) deleted the few lines that took advantage of the new methods in the basicSampling module. WIP on estimatedist. Currently runs but unsure if it is doing so properly

--- examples/estimateDist/estimatedist.py
+++ examples/estimateDist/estimatedist.py
@@ -35,14 +35,10 @@ sampler = bsam.sampler(my_model)
 # The partition set is drawn from a regular grid to represent 'possible observations'
 Partition_Set = samp.sample_set(dim_input)
 Partition_Set.set_domain(np.repeat([dim_range], dim_input, axis=0))
-
-# ONE OF TWO METHODS
-Partition_Set.regular_sample_set(Partition_Set, num_samples_per_dim = np.repeat(grid_cells_per_dim-1, dim_input, axis=0))
+Partition_Set = bsam.regular_sample_set(Partition_Set, num_samples_per_dim = np.repeat(grid_cells_per_dim-1, dim_input, axis=0))
 Partition_Discretization = sampler.compute_QoI_and_create_discretization(Partition_Set)
-# Partition_Discretization = sampler.create_regular_discretization(Partition_Set,
-#             num_samples_per_dim = np.repeat(grid_cells_per_dim-1, dim_input, axis=0))
 Partition_Discretization._input_sample_set.estimate_volume_mc() # The MC assumption is true.
-
+print 'Regular Reference Sample Set Done'
 
 # The emulated set is drawn from a given density to represent 'likely observations'
 # TODO add in functionality here to change the distribution - look at dim_range (maybe add 'support_range')
@@ -51,21 +47,31 @@ Emulated_Set.set_domain(np.repeat([dim_range], dim_input, axis=0))
 Emulated_Set.set_values(np.array( np.transpose([ np.random.beta(a=alpha, b=beta,
             size=num_samples_emulate_data_space) for i in range(dim_input) ]) ))
 Emulated_Discretization = sampler.compute_QoI_and_create_discretization(Emulated_Set)
-Emulated_Discretization._input_sample_set.estimate_volume(n_mc_points=num_samples_emulate_data_space*100) # The MC assumption is false.
-
+print 'Emulated Reference Sample Set Done'
 
 # Sample from parameter space
 Input_Samples = samp.sample_set(dim_input)
 Input_Samples.set_domain(np.repeat([dim_range], dim_input, axis=0))
-
-Input_Samples.random_sample_set('random', Input_Samples, num_samples = num_samples_param_space)
+Input_Samples = bsam.random_sample_set('random', Input_Samples, num_samples = num_samples_param_space)
 My_Discretization = sampler.compute_QoI_and_create_discretization(Input_Samples)
-# My_Discretization = sampler.create_random_discretization('random', Input_Samples, 
-#             num_samples = num_samples_param_space)
 MC_assumption = True
 # Estimate volumes of Voronoi cells associated with the parameter samples
 if MC_assumption is False:
     My_Discretization._input_sample_set.estimate_volume(n_mc_points=100*num_samples_param_space)
 else:
     My_Discretization._input_sample_set.estimate_volume_mc()
+print 'Experimental Input Sample Set Done'
+
+
+# Compute the simple function approximation to the distribution on the data space
+simpleFunP.user_partition_user_distribution(My_Discretization,
+                                            Partition_Discretization,
+                                            Emulated_Discretization)
+# Calculate probabilities
+calculateP.prob(My_Discretization)
+
+# visualize
+plotD.scatter_2D(My_Discretization)
+(bins, marginals2D) = plotP.calculate_2D_marginal_probs(Input_Samples, nbins = [20, 20])
+plotP.plot_2D_marginal_probs(marginals2D, bins, input_samples, filename = "3_Recovered_Distribution", file_extension = ".png", plot_surface=False)
 
