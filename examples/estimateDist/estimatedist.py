07a38684e6dba64561f6d702a8d08e0649d33446
M. Pilosov <michael.pilosov@ucdenver.edu>
2016-07-01 15:32:56 -0600

    WIP.

--- examples/estimateDist/estimatedist.py
+++ examples/estimateDist/estimatedist.py
@@ -13,10 +13,6 @@ import bet.sensitivity.gradients as grad
 import bet.sensitivity.chooseQoIs as cqoi
 import bet.Comm as comm
 
-################################################################################
-# FUNCTION DEFINITIONS
-################################################################################
-
 def Hellinger(A, B):
     """
     A = first reference measure (matrix from 2D marginal output)
@@ -67,28 +63,37 @@ def my_model(parameter_samples):
     QoI_samples = np.dot(parameter_samples, np.transpose(Q_map))
     return QoI_samples
 
-def identity_model(parameter_samples):
-    Q_map = np.array([[1.0, 0.0], [0.0, 1.0]])
-    QoI_samples = np.dot(parameter_samples, np.transpose(Q_map))
-    return QoI_samples
-################################################################################
-################################################################################
+def generate_reference(dim_input, grid_cells_per_dim, alpha, beta, save_disc = True, save_plot = False):
+    # Create Reference Discretization against which you will compare approximations with N samples
+    Reference_Set = samp.sample_set(dim_input)
+    Reference_Set.set_domain(np.repeat([dim_range], dim_input, axis=0))
+    Reference_Set = bsam.regular_sample_set(Reference_Set, num_samples_per_dim = np.repeat(grid_cells_per_dim, dim_input, axis=0))
+    Reference_Discretization = samp.discretization(Reference_Set, Reference_Set)
+    
+    num_samples_emulate_data_space = grid_cells_per_dim**dim_input*100;
+    
+    Emulated_Set = samp.sample_set(dim_input)
+    Emulated_Set.set_domain(np.repeat([[0.0, 1.0]], dim_input, axis=0))
+    Emulated_Set.set_values(np.array( np.transpose([ np.random.beta(a=alpha, b=beta,
+                size=num_samples_emulate_data_space) for i in range(dim_input) ]) ))
 
+    # Reference_Discretization._input_sample_set.estimate_volume_mc() # The MC assumption is true.
+    Reference_Emulation = eye.compute_QoI_and_create_discretization(Emulated_Set)
+    simpleFunP.user_partition_user_distribution(Reference_Discretization, 
+                                                Reference_Discretization, 
+                                                Reference_Emulation)
 
-# np.random.seed(20)
-# 
-# dim_input = 2 # dimension of paramater space
-# # dim_output = 3 # number of QoI
-# 
-# num_samples_param_space = 1E4 # this is N
-# grid_cells_per_dim = 10 # Discretizing the Data Space using a regular grid on the input space - this is = log_dim (M)
-# num_samples_emulate_data_space = (grid_cells_per_dim**dim_input)*100 # 1E4 # this is P (think about how many bins we're discretizing with)
-# 
+    Reference_Discretization._input_sample_set.set_probabilities(Reference_Discretization._output_probability_set._probabilities)
+    if save_disc == True:
+        samp.save_discretization(Reference_Discretization, file_name="0_(%d,%d)_M%d_Reference_Discretization"%(alpha, beta, grid_cells_per_dim ))
+    
+    (bins, ref_marginals2D) = plotP.calculate_2D_marginal_probs(Reference_Discretization._input_sample_set, nbins = grid_cells_per_dim)
+    if save_plot == True:
+        plotP.plot_2D_marginal_probs(ref_marginals2D, bins, Reference_Discretization._input_sample_set, 
+                filename = "1_(%d,%d)_M%d_Reference_Distribution"%(alpha, beta, grid_cells_per_dim), 
+                file_extension = ".png", plot_surface=False)
+    return
 
-# 
-# plotting_on = False
-# save_disc = False
-# MC_assumption = True # (for your input samples)
 
 def generate_data(num_samples_param_space, grid_cells_per_dim, alpha=1, beta=1, plotting_on = False, save_disc = False, MC_assumption = True ):
     # initialize some variables you might pass as parameters later on.
@@ -99,7 +104,6 @@ def generate_data(num_samples_param_space, grid_cells_per_dim, alpha=1, beta=1, 
     
     # Define the sampler that will be used to create the discretization object
     sampler = bsam.sampler(my_model)
-    eye = bsam.sampler(identity_model)
 
     # Initialize sample objects and discretizations that we will be using.
     # The partition set is drawn from a regular grid to represent 'possible observations'
@@ -116,40 +120,12 @@ def generate_data(num_samples_param_space, grid_cells_per_dim, alpha=1, beta=1, 
                 size=num_samples_emulate_data_space) for i in range(dim_input) ]) ))
 
 
-
-    # Create Reference Discretization against which you will compare approximations with N samples
-    Reference_Set = samp.sample_set(dim_input)
-    Reference_Set.set_domain(np.repeat([dim_range], dim_input, axis=0))
-    Reference_Set = bsam.regular_sample_set(Reference_Set, num_samples_per_dim = np.repeat(grid_cells_per_dim, dim_input, axis=0))
-    Reference_Discretization = eye.compute_QoI_and_create_discretization(Reference_Set)
-    Reference_Discretization._input_sample_set.estimate_volume_mc() # The MC assumption is true.
-    Reference_Emulation = eye.compute_QoI_and_create_discretization(Emulated_Set.copy())
-    simpleFunP.user_partition_user_distribution(Reference_Discretization, 
-                                                Reference_Discretization, 
-                                                Reference_Emulation)
-
-    Reference_Discretization._input_sample_set.set_probabilities(Reference_Discretization._output_probability_set._probabilities)
-    if save_disc == True:
-        samp.save_discretization(Reference_Discretization, file_name="0_(%d,%d)_M%d_Reference_Discretization"%(alpha, beta, grid_cells_per_dim ))
-    
-
-    (bins, ref_marginals2D) = plotP.calculate_2D_marginal_probs(Reference_Discretization._input_sample_set, nbins = grid_cells_per_dim)
-    if plotting_on == True:
-        plotP.plot_2D_marginal_probs(ref_marginals2D, bins, Reference_Discretization._input_sample_set, 
-                filename = "1_(%d,%d)_M%d_Reference_Distribution"%(alpha, beta, grid_cells_per_dim), 
-                file_extension = ".png", plot_surface=False)
-
-
     # Sample from parameter space
     Input_Samples = samp.sample_set(dim_input)
     Input_Samples.set_domain(np.repeat([dim_range], dim_input, axis=0))
-    ########################### DEBUG AREA #####################################
     Input_Samples = bsam.random_sample_set('random', Input_Samples, num_samples = num_samples_param_space)
-    # Input_Samples = bsam.regular_sample_set(Input_Samples, num_samples_per_dim = np.repeat(grid_cells_per_dim, dim_input, axis=0))
-    
-    plotD.scatter_2D(Reference_Discretization._output_sample_set, filename='ReferenceOutputs')
-    ############################################################################
     Input_Samples.estimate_volume_mc()
+    # Input_Samples.estimate_volume(n_mc_points=100*num_samples_param_space)
     My_Discretization = sampler.compute_QoI_and_create_discretization(Input_Samples)
 
     # These two objects are the ones we will use to construct our competing data-spaces.
@@ -158,50 +134,10 @@ def generate_data(num_samples_param_space, grid_cells_per_dim, alpha=1, beta=1, 
     Partition_Discretization = sampler.compute_QoI_and_create_discretization(Partition_Set)
     #Partition_Discretization._input_sample_set.estimate_volume_mc() # The MC assumption is true.
     Emulated_Discretization = sampler.compute_QoI_and_create_discretization(Emulated_Set)
+
     
-    # 
-    # # Estimate volumes of Voronoi cells associated with the parameter samples
-    # if MC_assumption is False:
-    #     My_Discretization._input_sample_set.estimate_volume(n_mc_points=10*num_samples_param_space)
-    # else:
-    #     My_Discretization._input_sample_set.estimate_volume_mc()
-    # print 'Experimental Input Sample Discretization Created'
-
-
-
-    # Calculate the gradient vectors at some subset of the samples (num_centers).
-    # Here the *normalize* argument is set to *True* because we are using bin_ratio to
-    # determine the uncertainty in our data.
-
-    # We will approximate the jacobian at each of the centers
-    '''
-    center_discretization = grad.calculate_gradients_rbf(My_Discretization,
-        num_centers, normalize=True)
-    print 'Gradients Computed'
-    '''
-    # With these gradient vectors, we are now ready to choose an optimal set of
-    # QoIs to use in the inverse problem, based on optimal skewness properites of
-    # QoI vectors.  This method returns a list of matrices.  Each matrix has output_dim rows
-    # the first column representing the average skewness of the Jacobian of Q,
-    # and the rest of the columns the corresponding QoI indices.
-
-    '''
-    input_samples_center = center_discretization.get_input_sample_set()
-    best_sets = cqoi.chooseOptQoIs_large(input_samples_center, measure=False)
-    print best_sets
-    print 'Best Sets Computed'
-    '''
-
-    # At this point we have determined the optimal set of QoIs to use in the inverse
-    # problem.  Now we compare the support of the inverse solution using
-    # different sets of these QoIs.
-
-
-
-    # print 'Reference Sample Discretizations Created, Reference Density Computed'
-    # print ref_marginals2D
-    
-    H = []
+    H = [] # vector to store Hellinger Distances.
+    # TODO enumerate all possible choices of QoI maps. -- or change my_model each time? 
     for i in range(2): # Possible sets of QoI to choose
         QoI_indices = [i, i+1] # choose up to input_dim
         # print 'QoI Pair %d'%(i+1)
@@ -213,7 +149,7 @@ def generate_data(num_samples_param_space, grid_cells_per_dim, alpha=1, beta=1, 
 
         (bins, marginals2D) = plotP.calculate_2D_marginal_probs(my_discretization._input_sample_set, nbins = grid_cells_per_dim)
         
-        if plotting_on == True:
+        if plotting_on == True & i == 0:
             plotP.plot_2D_marginal_probs(marginals2D, bins, my_discretization._input_sample_set, 
                             filename = "2_(%d,%d)_M%d_N%d_Recovered_Distribution_q(%d,%d)"%(alpha, beta, grid_cells_per_dim, num_samples_param_space, QoI_indices[0], QoI_indices[1]), 
                             file_extension = ".png", plot_surface=False)
