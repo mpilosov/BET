73501dd20b68b534dc847be3c8312d9a0f0f5635
M. Pilosov <michael.pilosov@ucdenver.edu>
2017-04-27 16:10:02 -0600

    only RBF remains in getgradients.

--- examples/heatrod/getgradients.py
+++ examples/heatrod/getgradients.py
@@ -9,48 +9,6 @@ import numpy as np
 from simulation_2kappas_setup import *
 from heatROD import *
 
-
-
-num_centers_grad = 50
-# create sample set for gradient computations
-grad_centers_sample_set = samp.sample_set(2)
-grad_centers_sample_set.set_domain( np.array([[0.01, 0.2],[0.01, 0.2]]) )
-grad_centers_sample_set = bsam.random_sample_set('random', 
-        grad_centers_sample_set, num_samples = num_centers_grad)
-cluster_input_set = bgrad.pick_cfd_points(grad_centers_sample_set, 0.0025*np.ones(2))
-
-# now that we've determined our clusters for the CFD, we need to run the heatrod
-# and for each sample, compute the functional values (temp at a point) 
-functional_values = np.zeros( (cluster_input_set.check_num(), len(temp_locs)) )
-# loop over each sample and call heatrod
-for i in range( cluster_input_set.check_num() ):
-    # print 'Sample : ', i, '(kappa_0, kappa_1) = ', cluster_input_set._values[i,:]
-    kappa_0, kappa_1 = cluster_input_set._values[i,:]
-    
-    T = heatROD(i, amp, px, width, degree, T_R, kappa_0, kappa_1, rho, cap, nx, mesh, dt, t_stop, saveopt=False)
-    # now that heatROD has been run for this specific combination of kappas, we 
-    # write the QoI values as the outputs in the discretization.
-    functional_values[i] = np.array([T(xi) for xi in temp_locs]) # take temperature values at points of interest
-
-cluster_output_set = samp.sample_set( len(temp_locs) )
-cluster_output_set.set_values(functional_values)
-cluster_discretization = samp.discretization(cluster_input_set, cluster_output_set) # combine into discretization
-# cluster_output_set = samp.sample_set(2)
-# cluster_discretization = samp.discretization(cluster_input_set, cluster_input_set) # combine into discretization
-
-center_discretization = bgrad.calculate_gradients_cfd(cluster_discretization) # finally, compute the gradients
-input_samples_centers = center_discretization.get_input_sample_set() # pull center values
-
-
-index1 = 0
-for index2 in range(1+index1,len(temp_locs)):
-    (specific_skewness, _) = cqoi.calculate_avg_skewness(input_samples_centers,
-            qoi_set=[index1, index2])
-    print 'The average skewness of the QoI map defined by indices (%2d, %2d)'%(index1, index2) + \
-        ' is ' + str(specific_skewness)
-
-
-
 matfile = sio.loadmat(filename)
 
 input_samples = samp.sample_set(2)
@@ -71,8 +29,3 @@ for index2 in range(1+index1,len(temp_locs)):
             qoi_set=[index1, index2])
     print 'The average skewness of the QoI map defined by indices (%2d, %2d)'%(index1, index2) + \
         ' is ' + str(specific_skewness)
-
-
-# # SAVE input_samples_centers
-# centers_filename = 'input_sample_centers_grads'
-# samp.save_sample_set(input_samples_centers,centers_filename)
