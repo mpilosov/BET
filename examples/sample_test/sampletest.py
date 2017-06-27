ea9b5f82fc8f5238d05e11f4428cba6dd25ad776
mpilosov_LANL <mpilosov@wf-fe2.lanl.gov>
2017-06-27 14:03:11 -0600

    testing username

--- examples/sample_test/sampletest.py
+++ examples/sample_test/sampletest.py
@@ -14,5 +14,6 @@ num_input_samples = int(1E4)
 input_samples = sampler.random_sample_set('random', input_samples, num_samples = num_input_samples)
 #input_samples.estimate_volume(n_mc_points=num_input_samples*50)
 input_samples.estimate_volume_mc()
+
 my_disc = sampler.compute_QoI_and_create_discretization(input_samples)
 
