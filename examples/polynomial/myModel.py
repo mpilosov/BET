7511cc776813c791f134eb50fa42f3069619eec0
Michael Pilosov <michael.pilosov@ucdenver.edu>
2017-06-28 10:47:51 -0600

    RSS in mymodel, ipynb changes.

--- examples/polynomial/myModel.py
+++ examples/polynomial/myModel.py
@@ -9,8 +9,9 @@ def my_model(parameter_samples):
     # rows are samples from parameter space lambda, columns are a,b,c,n
     # our model is evaluated at t = [1,2,3,4], which are the four cols of output
     A = parameter_samples
+    weights = 1.0/1.0
     QoI_samples = np.array([A[:,0]*(t**A[:,3]) + A[:,1]*t + A[:,2] for t in range(1,5)]).transpose()
-    return QoI_samples
+    return np.sum( ( (QoI_samples - np.array([1,2,3,4]) )**2 )*weights , 1) # RSS 
 
 def my_model2(parameter_samples):
     # model y(t) = a*t + c. unknown parameter lambda = [a, c]
@@ -35,3 +36,12 @@ def my_model4(parameter_samples):
     A = parameter_samples
     QoI_samples = np.array([A[:,0]*(t**A[:,3]) + A[:,1]*t + A[:,2] for t in range(1,5)]).transpose()
     return QoI_samples
+
+def griewank(parameter_samples):
+    n,d = parameter_samples.shape # num_samples, dimension
+    s = np.zeros(n) # sum
+    p = np.ones(n) # prod
+    for i in range(d):
+        s += parameter_samples[:,i]**2
+        p *= np.cos(parameter_samples[:,i]/np.sqrt(i+1))
+    return 1 + s/4000. - p
