e339e7a8a83d90dabd9de04bc9453885f4fbd0ab
M. Pilosov <consistentbayes@gmail.com>
2020-07-04 01:26:16 -0600

    renamed files.

--- examples/estimateDist/run.sh
+++ examples/estimateDist/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python estimatedist.py
 python estimatedist_build_pointers.py
-python estimatedist_post-process.py
-python estimatedist_plot-post-process.py
+python estimatedist_compute_metrics.py
+python estimatedist_plotdata.py
